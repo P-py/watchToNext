ef9d071a0a501cabceed8774a3f225c6c15f5008
Pedro S Santos <pedrosalviano170@gmail.com>
2026-05-13 00:28:45 -0300

    :wrench: ops[SMWTN-42]: enable CORS for browser frontend origins

--- backend/api/src/main/kotlin/com/watchtonext/api/ApiApplication.kt
+++ backend/api/src/main/kotlin/com/watchtonext/api/ApiApplication.kt
@@ -1,12 +1,13 @@
 package com.watchtonext.api
 
+import com.watchtonext.api.config.CorsProperties
 import com.watchtonext.api.config.RecommenderProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties(RecommenderProperties::class)
+@EnableConfigurationProperties(RecommenderProperties::class, CorsProperties::class)
 class ApiApplication
 
 fun main(args: Array<String>) {
