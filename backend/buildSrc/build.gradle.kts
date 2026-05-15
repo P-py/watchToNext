8650dfad0993e0108eb6dd15f30d9dd3f8ba5f3d
Pedro S Santos <pedrosalviano170@gmail.com>
2026-05-14 22:34:26 -0300

    :wrench: chore[build]: expose version catalog to buildSrc precompiled scripts

--- backend/buildSrc/build.gradle.kts
+++ backend/buildSrc/build.gradle.kts
@@ -8,4 +8,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-noarg:2.2.21")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:4.0.6")
     implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.7")
+    // Exposes the version catalog (libs) to precompiled script plugins.
+    implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
